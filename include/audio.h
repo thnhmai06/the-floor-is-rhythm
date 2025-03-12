859ef440b21318978dc981a1d15681dc693854ea
Mai Thành <62001770+thnhmai06@users.noreply.github.com>
2025-03-12 15:00:01 +0700

    init: vcpkg

--- include/audio.h
+++ include/audio.h
@@ -9,8 +9,8 @@
 #define AUDIO_H
 
 #include <unordered_map>
-#include <SDL.h>
-#include <SDL_mixer.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_mixer.h>
 
 //! Settings: (Low-offset Profile)
 //Volume
--- include/main.h
+++ include/main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <iostream>
-#include <SDL.h>
-#include <SDL_image.h>
-#include <SDL_ttf.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
 
 #endif // MAIN_H
