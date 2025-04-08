b69930705c97b1c87e2c227163f58ce571fde62c
Mai Thành <62001770+thnhmai06@users.noreply.github.com>
2025-04-08 22:46:35 +0700

    feat: Thêm PlayingScreen::Game

--- include/structures/screens/gameplay/playing.h
+++ include/structures/screens/gameplay/playing.h
@@ -6,6 +6,45 @@ namespace Structures::Render::Screen::Gameplay
 {
 	struct PlayingScreen : private Screens::Screen
 	{
+		std::unique_ptr<const BeatmapFile> beatmap;
+
+		struct Game
+		{
+			uint8_t health = 200;
+			int32_t time = -2000; // ms
+			struct Count
+			{
+				unsigned long count_300 = 0;
+				unsigned long count_100 = 0;
+				unsigned long count_50 = 0;
+				unsigned long count_miss = 0;
+
+				[[nodiscard]] float get_accuracy() const;
+			} count;
+			unsigned long long score = 0;
+			unsigned long combo = 0;
+			unsigned long max_combo = 0;
+			auto current_direction = Template::Game::Direction::Direction::RIGHT;
+			auto require_direction = Template::Game::Direction::Direction::RIGHT;
+
+			// optional
+			struct Keystroke
+			{
+				struct
+				{
+					unsigned long right = 0;
+					unsigned long left = 0;
+					unsigned long up = 0;
+					unsigned long down = 0;
+				} direction;
+				struct
+				{
+					unsigned long k1 = 0;
+					unsigned long k2 = 0;
+				} click;
+			} key_stoke;
+		} game;
+
 		struct Render : Screen::Render
 		{
 			struct Components
@@ -14,16 +53,17 @@ namespace Structures::Render::Screen::Gameplay
 				Screen::Render::Item cursor;
 
 				Components(RenderObjects::RenderObjectStorage* storage,
-					Layers::Layer* playground_layer, Layers::Layer* cursor_layer);
+					Layers::Layer* playground_layer, 
+					Layers::Layer* cursor_layer);
 			} components;
-			Template::Game::Direction::Direction current_direction = Template::Game::Direction::Direction::RIGHT;
 
-			Render(Layers::Layer& playground_layer,
+			Render(const Game& game,
+				Layers::Layer& playground_layer,
 				Layers::Layer& cursor_layer,
 				const Textures::TextureMemory& skin,
 				const BeatmapFile& beatmap);
 		} render;
 
-		explicit PlayingScreen(const BeatmapFile& beatmap);
+		explicit PlayingScreen(const char* beatmap_path);
 	};
 }
--- src/structures/screens/gameplay/playing.cpp
+++ src/structures/screens/gameplay/playing.cpp
@@ -1,4 +1,4 @@
-#include "structures/screens/gameplay/playing.h" // Header
+﻿#include "structures/screens/gameplay/playing.h" // Header
 #include "structures/render/layers/objects/gameplay/mapset.h"
 #include "structures/render/layers/objects/gameplay/cursor.h"
 #include "work/render/layers.h"
@@ -7,6 +7,20 @@
 using namespace Structures::Render::Screen::Gameplay;
 
 //! PLayingScreen
+// Game
+float PlayingScreen::Game::Count::get_accuracy() const
+{
+	if (count_300 + count_100 + count_50 + count_miss == 0) return 100.00f; // tránh chia 0
+
+	constexpr uint8_t SCORE_PERFECT = 30; // 300
+	constexpr uint8_t SCORE_GREAT = 10; // 100
+	constexpr uint8_t SCORE_BAD = 5; // 50
+
+	const unsigned long total = count_300 * SCORE_PERFECT + count_100 * SCORE_GREAT + count_50 * SCORE_BAD; // chia 10 để giảm số cần nhớ
+	const unsigned long total_if_perfect = (count_300 + count_100 + count_50 + count_miss) * SCORE_PERFECT;
+	return static_cast<float>(total) / static_cast<float>(total_if_perfect);
+}
+
 // Render
 PlayingScreen::Render::Components::Components(
 	RenderObjects::RenderObjectStorage* storage,
@@ -16,6 +30,7 @@ PlayingScreen::Render::Components::Components(
 }
 PlayingScreen::Render::Render
 (
+	const Game& game,
 	Layers::Layer& playground_layer,
 	Layers::Layer& cursor_layer,
 	const Textures::TextureMemory& skin,
@@ -34,13 +49,15 @@ PlayingScreen::Render::Render
 	using RenderObjects::Cursor::Collection::CursorCollection;
 	components.cursor.storage_item = storage.insert(
 		std::prev(storage.end(), 1),
-		std::make_unique<CursorCollection>(skin, &current_direction)
+		std::make_unique<CursorCollection>(skin, &game.current_direction)
 	);
 	components.cursor.render_item = cursor_layer.render_buffer.add_collection(components.cursor.storage_item->get());
 }
 
 // Total
-PlayingScreen::PlayingScreen(const BeatmapFile& beatmap) :
-	render(*Work::Render::Layers::playground, *Work::Render::Layers::cursor, *Work::Render::Textures::skin, beatmap)
+PlayingScreen::PlayingScreen(const char* beatmap_path) :
+	beatmap(std::make_unique<const BeatmapFile>(beatmap_path)),
+	render(game, *Work::Render::Layers::playground,
+		*Work::Render::Layers::cursor, *Work::Render::Textures::skin, *this->beatmap)
 {
 }
